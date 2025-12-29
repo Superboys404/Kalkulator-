d78a3ae21db115171cd74947fafa629be6655542
Superboys <113122314+IzziXGanzz@users.noreply.github.com>
2025-12-30 00:44:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+
+    if (!/^[0-9+\-*/. ]+$/.test(expression)) {
+        display.value = 'Input tidak valid';
+        return;
+    }
+
+    try {
+        display.value = eval(expression);
+    } catch {
+        display.value = 'Error nih';
+    }
+}
+
+
+function clearDisplay() {
+    const display =document.getElementById('display');
+    display.value = '';
+}
